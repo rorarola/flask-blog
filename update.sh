6e42fc6a3ea5e6df0ef2d6dc49219988ff5dbdda
youtube <youtube@rorarola-2.local>
2022-04-06 00:48:42 +0800

    2022-04-06 00:48:42

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
+# git add .
+# git commit -am "make it better"
+# git branch -M main
+# git remote add origin https://github.com/rorarola/flask-blog.git
+# git push -u origin main
+# git push heroku main
+
 
 # git remote add origin https://github.com/rorarola/flask-blog.git
 
